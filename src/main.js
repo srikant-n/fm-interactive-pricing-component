75df2727d940b5b2f7a66021c0d833f94f80e477
Srikant Nimmagadda <srikant.nims@gmail.com>
2021-02-11 11:43:17 +0530

    Add slider css using vendor prefix and basic toggle bar code

---
+++ src/main.js
@@ -0,0 +1,14 @@
+const SLIDER_COLOR_EMPTY = "hsl(224, 65%, 95%)";
+const SLIDER_COLOR_FULL = "hsl(174, 77%, 80%)";
+
+function updateSliderColor(slider) {
+    if(!slider)
+        return;
+    let value = (slider.value-slider.min)/(slider.max-slider.min)*100;
+    slider.style.background = `linear-gradient(to right, ${SLIDER_COLOR_FULL} 0%, ${SLIDER_COLOR_FULL} ${value}%, ${SLIDER_COLOR_EMPTY} ${value}%, ${SLIDER_COLOR_EMPTY} 100%)`;
+}
+
+document.querySelector(".price-slider").addEventListener('input',(event) => updateSliderColor(event.target));
+
+
+updateSliderColor(document.querySelector(".price-slider"));
